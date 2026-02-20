7cc096300370892220abb6ec3a44f99d7f2f39e6
Brandon Schurman <brandon@earthly.dev>
2026-02-20 15:32:40 -0500

    [DP-3] Add JVM version output to Hello

--- src/Hello.java
+++ src/Hello.java
@@ -1,5 +1,6 @@
 public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello from Ant!");
+        System.out.println("Build version: " + System.getProperty("java.version"));
     }
 }
